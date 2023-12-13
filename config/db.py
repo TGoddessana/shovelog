37e290aa3587b0a3b87d35df277a572a9715da5c
tgoddessana <twicegoddessana1229@gmail.com>
2023-12-14 01:01:26 +0900

    fix: db 세팅 변경

--- config/db.py
+++ config/db.py
@@ -1,12 +1,30 @@
-from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.orm import declarative_base
+from asyncio import current_task
 
+from sqlalchemy import MetaData
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncSession,
+    async_scoped_session,
+    async_sessionmaker,
+)
+from sqlalchemy.orm import declarative_base
 from config.settings import settings
 
 
 engine = create_async_engine(
-    settings.DB_URL,
-    echo=True,
+    settings.DB_URL, echo=True, connect_args={"check_same_thread": False}
 )
 
-Model = declarative_base()
+async_session_factory = async_sessionmaker(engine=engine, class_=AsyncSession)
+
+AsyncSession = async_scoped_session(async_session_factory, scopefunc=current_task)
+
+naming_convention = {
+    "ix": "%(column_0_label)s_idx",
+    "uq": "%(table_name)s_%(column_0_name)s_key",
+    "ck": "%(table_name)s_%(constraint_name)s_check",
+    "fk": "%(table_name)s_%(column_0_name)s_fkey",
+    "pk": "%(table_name)s_pkey",
+}
+
+Model = declarative_base(metadata=MetaData(naming_convention=naming_convention))
