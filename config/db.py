7c574c3e0b37dd0348cf8b0c9554f112d84825bb
tgoddessana <twicegoddessana1229@gmail.com>
2023-12-13 22:36:44 +0900

    feat: sqlalchemy alembic 세팅

--- config/db.py
+++ config/db.py
@@ -1,7 +1,12 @@
 from sqlalchemy.ext.asyncio import create_async_engine
-from sqlmodel import SQLModel
+from sqlalchemy.orm import declarative_base
+
 from config.settings import settings
 
 
-engine = create_async_engine(settings.DB_URL, echo=True)
-metadata = SQLModel.metadata
+engine = create_async_engine(
+    settings.DB_URL,
+    echo=True,
+)
+
+Model = declarative_base()
